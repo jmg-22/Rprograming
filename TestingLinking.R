819f463dbc91ec165bfa28862a6fca14e72044bb
jmg-22 <jerrinmg@gmail.com>
2020-07-23 10:22:44 +0530

    First you have to save on your local device then only can you Commit to the GitHub

--- TestingLinking.R
+++ TestingLinking.R
@@ -1,3 +1,9 @@
-print(" This following code is written in R programming")
+print(" This following code is written in R ")
 
-print(" And  now it lives on GitHUb")
+print(" And  now it lives on GitHUb ||  This has been Modified")
+
+
+print(" lEtme Try pullling pushing ")
+
+
+print(" lEtme Try pullling pushing andpushing as well as deleting ")
